8f59d0d80275cdd9b5c31dbb18fb344f0e02da4b
Sam <rz6hon@yandex.ru>
2015-11-14 23:38:23 +0300

    001-change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneFirstApp::Application.config.secret_key_base = '504eeb4f1c7cc67693142f85f683383b8bfffd36554981d574707c8778e2f277160954ca0d3071a66a788965653dce118d600a7b4e24660fa2892124e5f00844'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneFirstApp::Application.config.session_store :cookie_store, key: '_one-first_app_session'
